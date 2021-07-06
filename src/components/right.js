fb25cf12084b336c2ca9db47023252ae2baa5410
franchesca <ihuoma.franchesca@gmail.com>
2021-07-05 21:28:51 -0700

    Responsiveness

--- src/components/right.js
+++ src/components/right.js
@@ -17,13 +17,13 @@ class Right extends React.Component {
                             <h5 className="account">Sign in to continue to your account</h5>
                         <div className="email">
                             <label className="passwd">Email Address</label>
-                            <input type="text" placeholder="email address" value={this.state.email} />
+                            <input type="text" placeholder="Email address" value={this.state.email} />
                             </div>
                             <br />
 
                         <div className="password">
                             <label className="passwd"> Password
-                                <input type="password" placeholder="enter strong password" value={this.state.password} />
+                                <input type="password" placeholder="Enter strong password" value={this.state.password} />
                             </label>
                         </div>
                         <div className="btn"><button onClick={('')}>Sign in</button>
